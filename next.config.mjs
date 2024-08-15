87da5d62a20f4716a48c2db18f7c174abcde4b03
ceejaycodes <dreelceejay79@yahoo.com>
2024-08-15 07:44:33 +0100

    15/08/2024

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,21 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
-    images: {domains: ['images.unsplash.com']}
+    images: {domains: ['images.unsplash.com']},
+    poweredByHeader: false,
+    async headers() {
+        return [
+          {
+            source: '/:path*',
+            headers: [
+              { key: 'Access-Control-Allow-Credentials', value: 'true' },
+              { key: 'Access-Control-Allow-Origin', value: '*' },
+              { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+              { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+            ],
+          },
+        ];
+      },
 };
 
 export default nextConfig;
